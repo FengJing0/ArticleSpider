86c635238df40e06388df86df93e0243994c5bd4
FengJing <448195466@qq.com>
2019-01-09 17:22:36 +0800

    1.9

---
+++ article.sql
@@ -0,0 +1,36 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 100130
+Source Host           : localhost:3306
+Source Database       : article_spider
+
+Target Server Type    : MYSQL
+Target Server Version : 100130
+File Encoding         : 65001
+
+Date: 2019-01-09 17:11:47
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `title` varchar(255) NOT NULL,
+  `create_date` date DEFAULT NULL,
+  `url` varchar(300) NOT NULL,
+  `url_object_id` varchar(50) NOT NULL,
+  `front_image_url` varchar(300) DEFAULT NULL,
+  `front_image_path` varchar(255) DEFAULT NULL,
+  `praise_nums` int(11) NOT NULL DEFAULT '0',
+  `fav_nums` int(11) NOT NULL DEFAULT '0',
+  `comment_nums` int(11) NOT NULL DEFAULT '0',
+  `tags` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `content` longtext,
+  PRIMARY KEY (`url_object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
